8b1fac83816fda8cd55d1c20041badd3c4898c0b
shailyrani2512 <dummy@gmail.com>
2026-03-27 06:21:51 +0000

    controller implemented

--- server/src/main/java/com/edutech/supply_of_goods_management/service/UserService.java
+++ server/src/main/java/com/edutech/supply_of_goods_management/service/UserService.java
@@ -1,26 +1,39 @@
+// UserService.java
 package com.edutech.supply_of_goods_management.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 import com.edutech.supply_of_goods_management.entity.User;
 import com.edutech.supply_of_goods_management.repository.UserRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
-    public UserDetails loadUserByUsername(String username) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
     }
 
-    // implement the user service here
+    public User getByUsername(String username) {
+        return repo.findByUsername(username).orElse(null);
+    }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = repo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singleton(() -> user.getRole())
+        );
+    }
 }
